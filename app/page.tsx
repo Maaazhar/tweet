12ebb7c270f8f2c0d31e05925f273acc98a3056f
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-07-25 18:03:25 +0400

    Adding Tweet Table

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,12 @@ export default async function Home() {
   return <>
     <AuthButtonServer />
     <NewTweet/>
-    <pre>{JSON.stringify(tweets, null, 2)}</pre>
+    {/* <pre>{JSON.stringify(tweets, null, 2)}</pre> */}
+    {tweets?.map(tweet =>(
+      <div key={tweet.id}>
+        <p>{tweet?.profiles?.name} {tweet?.profiles?.user_name}</p>
+        <p>{tweet.title}</p>
+      </div>
+    ))}
   </>
 }
