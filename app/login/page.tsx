d224944169113b5db253d6a18ad747e344c3777b
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-07-29 19:50:03 +0400

    Making all the components force dynamic that uses cookies for dynamic server usage

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,6 +4,8 @@ import AuthButtonClient from "../auth-button-client";
 import GithubButton from "./github-button";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function Login() {
   const supabase = createServerComponentClient<Database>({ cookies })
   const { data: { session } } = await supabase.auth.getSession()
