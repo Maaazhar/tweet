29e9ab30bc2067839216bd478d405e909fb3327a
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-08-07 13:44:37 +0400

    adding clock logo in tweet component

--- app/tweets.tsx
+++ app/tweets.tsx
@@ -129,8 +129,23 @@ export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
             </p>
             {/* <time dateTime={tweet.created_at}>{tweet.created_at}</time> */}
           </div>
-          <div>
-            <p className="text-sm text-slate-500">Created on {convertToUserTimeZone(tweet.created_at)}</p>
+          <div className="flex items-center gap-x-1">
+            {/* Created on  */}
+            <svg xmlns="http://www.w3.org/2000/svg"
+              className="text-slate-500"
+              width="15" height="15"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <circle cx="12" cy="12" r="10"></circle>
+              <polyline points="12 6 12 12 16 14"></polyline>
+            </svg>
+            <p className="text-sm text-slate-500">
+              {convertToUserTimeZone(tweet.created_at)}</p>
           </div>
         </div>
       </div>
