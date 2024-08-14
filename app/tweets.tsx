340dfbfc56bda1b3cbc5947905681c878f0a884a
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-08-14 18:28:51 +0400

    updating user card in user page-15(adding edit button)

--- app/tweets.tsx
+++ app/tweets.tsx
@@ -50,6 +50,10 @@ export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
     router.refresh();
   };
 
+  const handleEdit = async (id: string) => {
+    router.refresh();
+  };
+
   //converting time in to user timezone
   const convertToUserTimeZone = (dateString: string): string => {
     // Parse the date string into a Date object
@@ -156,32 +160,51 @@ export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
         </div>
         <div>
           {tweet.optionButton &&
-            <div>
-              <button className="text-md font-medium text-slate-500 p-1.5 rounded-full transition-all duration-300 ease-in-out hover:shadow-[0px_0px_15px] hover:shadow-red-600 hover:bg-transparent"
-                  title="Click to see the options.">
-                <div className="flex gap-1">
-                  <span className="size-1 bg-slate-500 rounded-full"></span>
-                  <span className="size-1 bg-slate-500 rounded-full"></span>
-                  <span className="size-1 bg-slate-500 rounded-full"></span>
+            <div className="flex flex-col items-end">
+              <button className="group size-8 flex justify-center items-center rounded-full transition-all duration-300 ease-in-out hover:shadow-[0px_0px_15px] hover:shadow-sky-500/50 hover:bg-transparent"
+                title="Click to see the options.">
+                <div className="flex gap-0.5">
+                  <span className="size-1 bg-slate-500 rounded-full group-hover:bg-sky-600"></span>
+                  <span className="size-1 bg-slate-500 rounded-full group-hover:bg-sky-600"></span>
+                  <span className="size-1 bg-slate-500 rounded-full group-hover:bg-sky-600"></span>
                 </div>
               </button>
-              <div>
+              <div className="mt-4 p-2 flex flex-col gap-1 rounded-md border border-sky-500/20 transition-all duration-300 ease-in-out shadow-[0px_0px_15px] shadow-sky-500/30 ">
                 <button
-                  className="text-md font-medium text-slate-500 p-1.5 rounded-full transition-all duration-300 ease-in-out hover:shadow-[0px_0px_15px] hover:shadow-red-600 hover:bg-transparent hover:text-red-600"
+                  className="w-full flex gap-2 justify-start items-center text-md font-medium text-slate-500 p-1.5 rounded-md transition-all duration-300 ease-in-out hover:bg-red-600/10 hover:text-red-600"
                   title="Click to delete this tweet. Once deleted it can't be retrieved."
                   onClick={() => handleDelete(tweet.id)}>
-                  {/* <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-5 ">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                </svg> */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-5 ">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                  </svg>
+                  <span>Delete</span>
+                </button>
+                <button
+                  className="w-full flex gap-2 justify-start items-center text-md font-medium text-slate-500 p-1.5 rounded-md transition-all duration-300 ease-in-out hover:bg-sky-600/10 hover:text-sky-600"
+                  title="Click to edit this tweet."
+                  onClick={() => handleEdit(tweet.id)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-5">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                  </svg>
+                  <span>Edit</span>
                 </button>
               </div>
             </div>}
